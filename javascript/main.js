a56f8ed933f1e6170dbce82d728dab86a62bf20a
Allbertss <42111918+Allbertss@users.noreply.github.com>
2022-02-23 21:03:29 +0200

    updated files

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ $("header ul a").on("click", function (e) {
     e.preventDefault();
     const href = $(this).attr("href");
     $("html, body").animate({ 
-        scrollTop: $(href).offset().top - 50 }, 1000);
+        scrollTop: $(href).offset().top - 100 }, 1000);
     }
 );
 
@@ -16,16 +16,28 @@ $(window).scroll(function(){
 });
 
 // home, cycle images
-let imageIndex = 6;
+let imageIndex = 4;
 window.onload = function() {
     setInterval(change, 3500);
+    setInterval(changeAboutUs, 3500);
 }
 
 function change() {
     if (imageIndex == 0)
-        imageIndex = 6;
+        imageIndex = 4;
 
     $('#home-image').attr('src', 'images/interior' + imageIndex + '.jpg');
     
     imageIndex--;
+}
+
+// about-us, cycle images
+let imageIndex2 = 2;
+function changeAboutUs() {
+    if (imageIndex2 == 0)
+        imageIndex2 = 2;
+
+    $('#about-us-image').attr('src', 'images/interior-f' + imageIndex2 + '.jpg');
+    
+    imageIndex2--;
 }
