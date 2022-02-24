d9ffc93d460374b0848daa83e7704c64374e465a
Allbertss <42111918+Allbertss@users.noreply.github.com>
2022-02-24 02:36:53 +0200

    major updates

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ $("header ul a").on("click", function (e) {
     e.preventDefault();
     const href = $(this).attr("href");
     $("html, body").animate({ 
-        scrollTop: $(href).offset().top - 100 }, 1000);
+        scrollTop: $(href).offset().top - 125 }, 1000);
     }
 );
 
@@ -14,33 +14,6 @@ $(window).scroll(function(){
     var scrolled = (scroll / height) * 100;
     document.getElementById("progress-bar").style.width = scrolled + "%";
 });
-
-// // home, cycle images
-// let imageIndex = 4;
-// window.onload = function() {
-//     setInterval(change, 5000);
-//     setInterval(changeAboutUs, 5000);
-// }
-
-// function change() {
-//     if (imageIndex == 0)
-//         imageIndex = 4;
-
-//     $('#home-image').attr('src', 'images/interior' + imageIndex + '.jpg');
-    
-//     imageIndex--;
-// }
-
-// // about-us, cycle images
-// let imageIndex2 = 2;
-// function changeAboutUs() {
-//     if (imageIndex2 == 0)
-//         imageIndex2 = 2;
-
-//     $('#about-us-image').attr('src', 'images/interior-f' + imageIndex2 + '.jpg');
-    
-//     imageIndex2--;
-// }
 
 // home gallery
 let galleryIndex = 1;
@@ -63,4 +36,26 @@ function showImage(value) {
         gallery[i].style.display = 'none';
 
     gallery[galleryIndex - 1].style.display = 'block';
+}
+
+// about-us gallary
+let galleryIndex2 = 1;
+showImage2(galleryIndex2);
+function changeIndex2(value) {
+    showImage2(galleryIndex2 += value);
+}
+
+function showImage2(value) {
+    let gallery2 = $('.gallery2');
+
+    if (value > gallery2.length)
+        galleryIndex2 = 1;
+
+    if (value < 1)
+        galleryIndex2 = gallery2.length;
+
+    for (let i = 0; i < gallery2.length; i++)
+        gallery2[i].style.display = 'none';
+
+    gallery2[galleryIndex2 - 1].style.display = 'block';
 }
